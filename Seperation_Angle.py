5c1ae6a554ddcf03cc89d2f100479cc223b6ac61
S Jagadeesh <140708404+jagadeesh183@users.noreply.github.com>
2025-01-08 21:12:42 +0530

    Update Seperation_Angle.py

--- Seperation_Angle.py
+++ Seperation_Angle.py
@@ -60,7 +60,8 @@ def display_form():
     observation_duration = st.number_input("Observation Duration (in hours)", min_value=0.0, step=0.1)
     threshold_angle = st.number_input("Threshold Separation Angle (degrees)", min_value=0.0, step=0.1)
     observatory_name = st.selectbox("Select Observatory", ["GMRT", "VLA"])
-    summary_file = create_or_clear_directory(outputfolder)
+    
+    summary_file = create_or_clear_directory(OUTPUT_DIR)
     with open(summary_file, 'a') as file:
         file.write(f"Observatory Name: {observatory_name} \n")
         file.write(f"Start Time: {start_time_ist} \n")
