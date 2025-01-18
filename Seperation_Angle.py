7349a306c0e44cc3aac1eccbdf8c95c9d55b4156
S Jagadeesh <140708404+jagadeesh183@users.noreply.github.com>
2025-01-19 00:29:53 +0530

    Update Seperation_Angle.py

--- Seperation_Angle.py
+++ Seperation_Angle.py
@@ -258,7 +258,7 @@ def display_footer():
                 <!-- Social Media Icons -->
     <div style="margin-top: 10px; display: flex; justify-content: flex-start; align-items: center; gap: 10px;">
         <a href="https://www.facebook.com/indianpta/" target="_blank" style="text-decoration: none;">
-            <img src="https://download.logo.wine/logo/Facebook/Facebook-f_Logo-Black-Logo.wine.png" alt="Facebook" style="width: 44px; height: 24px;">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook" style="width: 44px; height: 24px;">
         </a>
         <a href="https://x.com/InPTA_GW" target="_blank" style="text-decoration: none;">
             <img src="https://avatars.githubusercontent.com/u/50278?s=200&v=4" alt="Twitter" style="width: 24px; height: 24px;">
