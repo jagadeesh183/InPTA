43073e9976598d02f87275fa25dc2f32a84e621f
S Jagadeesh <140708404+jagadeesh183@users.noreply.github.com>
2025-01-27 22:56:48 +0530

    Update Seperation_Angle.py

--- Seperation_Angle.py
+++ Seperation_Angle.py
@@ -18,7 +18,7 @@ st.set_page_config(
 development_mode_css = """
     <style>
     .development-banner {
-        position: down;
+        position: bottom;
         top: 0;
         left: 0;
         width: 100%;
